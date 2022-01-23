8d75d9027bc14b121e01d551d0470747ddb02899
zordtk <zordxyz@protonmail.com>
2022-01-23 15:43:09 -0500

    Use cksfv

--- rescepy/cfv.py
+++ rescepy/cfv.py
@@ -10,14 +10,14 @@ class CFV(object):
     def __init__(self, binary=None):
         if binary == None:
             if os.name == 'posix':
-                self.binary = '/usr/bin/cfv'
+                self.binary = 'cksfv'
             elif os.name == 'nt':
                 self.binary = 'cfv'
         else:
             self.binary = binary
 
     def verify(self, opts=''):
-        sp = subprocess.Popen('%s -vsn %s -t sfv' % (self.binary, opts), shell=True, stdin=subprocess.PIPE)
+        sp = subprocess.Popen('%s %s' % (self.binary, opts), shell=True, stdin=subprocess.PIPE)
         sp.communicate()
 
         return True if sp.returncode == 0 else False
